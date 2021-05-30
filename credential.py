9a3725661f92f05b7743095cd7c4c3c637a956ce
winniediana4 <deinawinnie84@gmail.com>
2021-05-30 17:25:06 +0300

    add find user-name method

--- credential.py
+++ credential.py
@@ -17,13 +17,23 @@ class Credential:
      """  
      Credential.credential_list.append(self)
 
+
    @classmethod
    def display_credential(cls):
       """
-      display_credential method returns the credential_list
+      display_credential method displays the credential_list
       """
+
    def delete_credential(self):
       """
-      delete_credential method deletes objects from the credential_list
+      delete_credential method deletes saved objects from the credential_list
       """
       Credential.credential_list.remove(self)
+
+
+   @classmethod
+   def find_user_name(cls,user):
+      """
+      find_user_name method returns a credential that matches the user_name
+      """
+      return cls.credential_list
