ebfd26af388c1959b53fe15cf864b7ed2922a1f7
winniediana4 <deinawinnie84@gmail.com>
2021-05-30 18:31:06 +0300

    add test-init method

--- credential_test.py
+++ credential_test.py
@@ -16,3 +16,13 @@ class TestCredential(unittest.TestCase):
     """
     tearDown method that does clean up after each test case has run
     """
+    Credential.credential_list = []
+
+  def test_init(self):
+    """
+    test_init test case to test if the object is initialized properly
+    """
+    self.assertEqual(self.new_credential.view_password,"winnie")
+    self.assertEqual(self.new_credential.account,"facebook")
+    self.assertEqual(self.new_credential.login,"deinawinnie")
+    self.assertEqual(self.new_credential.password,"winnie")
