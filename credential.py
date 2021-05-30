e81b41135a74a06334c3146352639d74eac934f8
winniediana4 <deinawinnie84@gmail.com>
2021-05-30 16:35:58 +0300

    add--init-- method

--- credential.py
+++ credential.py
@@ -2,3 +2,8 @@ class Credential:
    """
    Class that generates new instances of a user's credentials
    """
+
+   credential_list=[]
+
+   def __init__(self,view_password,account,login,password):
+       pass
