2adf16993467743dc56e92738d17a30ae27e3d59
winniediana4 <deinawinnie84@gmail.com>
2021-05-30 18:57:19 +0300

    add test-save-credential

--- credential_test.py
+++ credential_test.py
@@ -26,3 +26,19 @@ class TestCredential(unittest.TestCase):
     self.assertEqual(self.new_credential.account,"facebook")
     self.assertEqual(self.new_credential.login,"deinawinnie")
     self.assertEqual(self.new_credential.password,"winnie")
+
+  def test_save_credential(self):
+    """
+    test_save_credential test case to test if the credential object is saved into
+    """
+    self.new_credential.save_credential()
+    self.assertEqual(len(Credential.credential_list),1)
+
+  def test_save_multiple_credential(self): 
+    """
+    test_save_multiple_credential to check if multiple credentials can be saved
+    """
+    self.new_credential.save_credential()
+    test_credential = Credential("winnie","test","login","winnie")
+    test_credential.save_credential()
+    self.assertEqual(len(Credential.credential_list),2)
