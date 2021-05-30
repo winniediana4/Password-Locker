2e331094b046183331ac768326458c3840b40f27
winniediana4 <deinawinnie84@gmail.com>
2021-05-30 17:52:43 +0300

    add set-up method

--- credential_test.py
+++ credential_test.py
@@ -1,2 +1,12 @@
 import unittest
 from credential import Credential
+
+class TestCredential(unittest.TestCase):
+  """
+
+  """
+
+  def setUp(self):
+    """
+    set_up method to run before each test case
+    """
